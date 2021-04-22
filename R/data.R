7d8d4fea1344c417e0fd578b16bfc383c0867823
Adam Davis <davisadamw@gmail.com>
2021-04-21 17:43:25 -0700

    add data

--- R/data.R
+++ R/data.R
@@ -31,7 +31,7 @@
 #' electric vehicle out-of-home charging demand to changes in light-duty vehicle
 #' fleet makeup and usage, a case study for California 2030"
 #'
-#' @format A named of two data frames with model coefficients, with entries
+#' @format A named list of two data frames with model coefficients, with entries
 #'   named `BEV` and `PHEV`. Each data frame has 8 rows and 2 variables:
 #'   \describe{ \item{Variable}{Variable name, all but intercept, p, and q must
 #'   match the name of a column in the block group dataset}
@@ -39,3 +39,27 @@
 #'   rate} }
 "assignment_model_coefficients"
 
+#' Commute OD matrix for the DVRPC region
+#'
+#' A dataset containing a commute OD matrix for the Delaware Valley region.
+#'
+#' @format A data frame with 1,879,017 rows and 4 variables: \describe{
+#' \item{Home_BlkGrp}{FIPS code for home/origin block group}
+#' \item{Work_BlkGrp}{FIPS code for work/destination block group}
+#' \item{Distance}{Distance between home and work block group, in miles}
+#' \item{total_Jobs}{Total jobs in `Work_BlkGrp` held by people who live in `Home_BlkGrp`}}
+#'
+#' @source Derived from the US Census LODES dataset.
+"dvrpc_od"
+
+#' Example output from `assign_evs()`
+#'
+#' A 100 block group random sample of an output from `assign_evs()` with 20,000 BEVs and 10,000 PHEVs assigned regionwide.
+#'
+#' @format A data frame with 100 rows and 3 variables: \describe{
+#' \item{GEOID}{FIPS code for each home block group of these vehicles}
+#' \item{bevs_20k}{Total number of BEVs in this block group under a 20,000 BEV scenario}
+#' \item{phevs_10k}{Total number of PHEVs in this block group under a 10,000 PHEV scenario}}
+#'
+#' @source Produced from this data using the `assign_evs()` function`
+"ev_totals_sample"
