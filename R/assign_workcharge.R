e0c50dfe8d889e34a717f7cec5afa9817e40a1ae
Adam Davis <davisadamw@gmail.com>
2021-04-23 18:08:18 -0700

    make_workcharge_probs now functional

--- R/assign_workcharge.R
+++ R/assign_workcharge.R
@@ -123,10 +123,129 @@ make_workcharge_probs <- function(use_BEV = TRUE,
     rate_paid <- 25
   }
   if(!is.logical(use_BEV)) stop("use_BEV should be either TRUE for BEV or FALSE for PHEV")
-  if(!is.logical(work_free)) stop("work_free should be either TRUE for free or FALSE for costly")
+  if(!is.logical(is_workcharge_free)) stop("is_workcharge_free should be either TRUE for free or FALSE for costly")
 
-  # grab the appropriate starting dataset and cut it down to potential values
-  if (isTRUE(use_BEV)) all_probs <- evworkplace::charge_probs_bev
-  else all_probs <- evworkplace::charge_probs_phev
+  # convert the weights to fractions
+  tot_rng_wt <- long_range_wt + medium_range_wt + short_range_wt
+  lrw <- long_range_wt   / tot_rng_wt
+  mrw <- medium_range_wt / tot_rng_wt
+  srw <- short_range_wt  / tot_rng_wt
+
+  # grab the appropriate starting dataset and cut it down to potential values ... account for basic range stuff here too
+  if (isTRUE(use_BEV)) {
+    range_cuts <- sort(unique(charge_probs_bev$actualrange))
+
+    # use only the top three ranges for BEVs
+    all_probs <- charge_probs_bev %>%
+      dplyr::mutate(range_wt = dplyr::case_when(.data$actualrange == max(range_cuts) ~ lrw,
+                                                .data$actualrange == range_cuts[3]   ~ mrw,
+                                                .data$actualrange == range_cuts[2]   ~ srw,
+                                                TRUE                                 ~ 0)) %>%
+      dplyr::group_by(.data$location, .data$dwell_type, .data$cmtdist, .data$rate_type,
+                      .data$work_free, .data$rate_paid) %>%
+      dplyr::summarise(charge_prob = sum(.data$range_wt * .data$charge_prob),
+                       .groups = "drop")
+  } else {
+    range_cuts <- sort(unique(charge_probs_phev$erange))
+
+    # use the top two ranges for PHEVs and combine the next two to make the short range
+    all_probs <- charge_probs_phev %>%
+      dplyr::mutate(range_wt = dplyr::case_when(.data$erange == max(range_cuts) ~ lrw,
+                                                .data$erange == range_cuts[4]   ~ mrw,
+                                                .data$erange == range_cuts[3]   ~ srw / 2,
+                                                .data$erange == range_cuts[2]   ~ srw / 2,
+                                                TRUE                            ~ 0)) %>%
+      dplyr::group_by(.data$location, .data$dwell_type, .data$cmtdist, .data$rate_type,
+                      .data$work_free, .data$rate_paid) %>%
+      dplyr::summarise(charge_prob = sum(.data$range_wt * .data$charge_prob),
+                       .groups = "drop")
+  }
+
+  # subset charging locations
+  probs_simplified_location <- all_probs %>%
+    # drop no-charge days
+    # for now, we're assuming everyone's on the middle rate category
+    # also grab only the appropriate work free / cost category
+    # also grab only single family detached and mud
+    dplyr::filter(.data$rate_type == 2,
+                  .data$location != 0,
+                  .data$work_free == is_workcharge_free,
+                  .data$dwell_type == 1 | .data$dwell_type == 3) %>%
+    # double count days where multiple charging locations used for total events
+    # home onlys will get switched to work for people without home charging
+    # count 70% of multi-location work charge events as work-home or work-public (the rest are home-public)
+    dplyr::mutate(all_events  = dplyr::if_else(.data$location == 4, 2 * .data$charge_prob, .data$charge_prob),
+                  home_onlys  = (.data$location == 1) * .data$charge_prob,
+                  work_events = dplyr::case_when(.data$location == 2 ~ .data$charge_prob,
+                                                 .data$location == 4 ~ 0.7 * .data$charge_prob,
+                                                 TRUE                ~ 0)) %>%
+    dplyr::group_by(.data$dwell_type, .data$cmtdist, .data$rate_paid) %>%
+    dplyr::summarize(tot_events  = sum(.data$all_events),
+                     home_onlys  = sum(.data$home_onlys),
+                     work_events = sum(.data$work_events),
+                     .groups = "drop")
+
+  # compute weights for electricity rate by interpolating two nearest values
+  all_rates <- unique(probs_simplified_location$rate_paid)
+  if (rate_paid > max(all_rates)) {
+
+    # if rate_used is above max, just use max
+    probs_simplified_rate <- probs_simplified_location %>%
+      dplyr::filter(.data$rate_paid == max(all_rates)) %>%
+      dplyr::select(-.data$rate_paid)
+
+  } else if (rate_paid < min(all_rates)) {
+
+    # if rate_used is below min, just use min
+    probs_simplified_rate <- probs_simplified_location %>%
+      dplyr::filter(.data$rate_paid == min(all_rates)) %>%
+      dplyr::select(-.data$rate_paid)
+
+  } else if (rate_paid %in% all_rates) {
+
+    # if rate_used is in the computed data, just use that
+    rp <- rate_paid
+    probs_simplified_rate <- probs_simplified_location %>%
+      dplyr::filter(.data$rate_paid == rp) %>%
+      dplyr::select(-.data$rate_paid)
+
+  } else {
+
+    # otherwise linearly interpolate between the two nearest computed rates
+    lo_rate <- max(all_rates[all_rates < rate_paid])
+    hi_rate <- min(all_rates[all_rates > rate_paid])
+    lo_wt   <- (hi_rate - rate_paid) / (hi_rate - lo_rate)
+
+    probs_simplified_rate <- probs_simplified_location %>%
+      dplyr::mutate(rate_weight = dplyr::case_when(
+        .data$rate_paid == lo_rate ~ lo_wt,
+        .data$rate_paid == hi_rate ~ 1 - lo_wt,
+        TRUE                       ~ 0)) %>%
+      dplyr::group_by(.data$dwell_type, .data$cmtdist) %>%
+      dplyr::summarise(tot_events  = sum(.data$tot_events  * .data$rate_weight),
+                       home_onlys  = sum(.data$home_onlys  * .data$rate_weight),
+                       work_events = sum(.data$work_events * .data$rate_weight),
+                       .groups = "drop")
+
+  }
+
+  probs_simplified_rate %>%
+    # apply home charge rate by housing type (move home-only days to work)
+    # separate home charging access rates by dwell type
+    # then apply single family home % weighting
+    dplyr::mutate(work_events_combi = .data$work_events +
+                    dplyr::if_else(.data$dwell_type == 1,
+                                   .data$home_onlys * (1 - sfh_homecharge),
+                                   .data$home_onlys * (1 - mud_homecharge)),
+                  dwell_wt =
+                    dplyr::if_else(.data$dwell_type == 1,
+                                   single_family_home_frac,
+                                   1 - single_family_home_frac),
+                  tot_events_wtd  = .data$tot_events * .data$dwell_wt,
+                  work_events_wtd = .data$work_events_combi * .data$dwell_wt) %>%
+    dplyr::group_by(.data$cmtdist) %>%
+    dplyr::summarise(tot_events = sum(.data$tot_events_wtd),
+                     work_events = sum(.data$work_events_wtd),
+                     .groups = "drop")
 
 }
